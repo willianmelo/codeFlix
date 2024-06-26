204e609e34607b5cdccf03bd50db7fdfc0e276a8
Willian Melo <williangilson_wg@hotmail.com>
2024-06-26 10:57:56 -0300

    adding agrregate root

--- CodeFlixCatalog/CatalogCodeFlix.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ CodeFlixCatalog/CatalogCodeFlix.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -1,4 +1,6 @@
 ﻿using CodeFlix.Catalog.Domain.Exceptions;
+using System;
+using System.Xml.Linq;
 using Xunit;
 using DomainEntity = CodeFlix.Catalog.Domain.Entity;
 
@@ -81,5 +83,104 @@ namespace CodeFlix.Catalog.UnitTests.Domain.Entity.Category
             Assert.Equal("Description should not be empty or null", exception.Message);
         }
 
+        [Theory(DisplayName = nameof(InstantiateErrorWhenNameHasLessThen3Characters))]
+        [Trait("Domain", "Category - Aggregates")]
+        [InlineData("1")]
+        [InlineData("a")]
+        [InlineData("ab")]
+        public void InstantiateErrorWhenNameHasLessThen3Characters(string name)
+        {
+            Action action =
+              () => new DomainEntity.Category(name, "Category Description");
+
+            var exception = Assert.Throws<EntityValidationException>(() => action());
+            Assert.Equal("Name should has at least 3 characters", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(InstantiateErrorWhenNameGreaterThen3Characters))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void InstantiateErrorWhenNameGreaterThen3Characters()
+        {
+            var invalidName = String.Join(null, Enumerable.Range(0, 256).Select(_ => "a").ToArray());
+
+            Action action =
+                () => new DomainEntity.Category(invalidName, "Category Description");
+
+            var exception = Assert.Throws<EntityValidationException>(() => action());
+            Assert.Equal("Name should has less then 255 characters", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(InstantiateErrorWhenDescriptionGreaterThen10000Characters))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void InstantiateErrorWhenDescriptionGreaterThen10000Characters()
+        {
+            var invalidDescription = String.Join(null, Enumerable.Range(0, 10001).Select(_ => "a").ToArray());
+
+            Action action =
+                () => new DomainEntity.Category("Category Name", invalidDescription);
+
+            var exception = Assert.Throws<EntityValidationException>(() => action());
+            Assert.Equal("Description should has less then 10.000 characters", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(Activate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Activate()
+        {
+            var validData = new
+            {
+                Name = "category name",
+                Description = "category description"
+            };
+
+            var category = new DomainEntity.Category(validData.Name, validData.Description, true);
+            category.Activate();
+
+            Assert.True(category.IsActive);
+        }
+
+        [Fact(DisplayName = nameof(Deactivate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Deactivate()
+        {
+            var validData = new
+            {
+                Name = "category name",
+                Description = "category description"
+            };
+
+            var category = new DomainEntity.Category(validData.Name, validData.Description, true);
+            category.Deactivate();
+
+            Assert.False(category.IsActive);
+        }
+
+        [Fact(DisplayName = nameof(Update))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Update()
+        {
+            var category = new DomainEntity.Category("Category Name", "Category Description");
+            var newValues = new { Name = "New Name", Description = "New Description" };
+
+            category.Update(newValues.Name, newValues.Description);
+
+            Assert.Equal(newValues.Name, category.Name);
+            Assert.Equal(newValues.Description, category.Description);
+        }
+
+
+        [Fact(DisplayName = nameof(UpdateOnlyName))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void UpdateOnlyName()
+        {
+            var category = new DomainEntity.Category("Category Name", "Category Description");
+            var newValues = new { Name = "New Name" };
+            var currentDescription = category.Description;
+
+            category.Update(newValues.Name);
+
+            Assert.Equal(newValues.Name, category.Name);
+            Assert.Equal(currentDescription, category.Description);
+        }
     }
 }
